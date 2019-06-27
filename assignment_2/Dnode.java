2b0426fbe57a8799d40088d70d9391de4e6d531c
chungcl <chungcl@ea71.eecs.yorku.ca>
2019-06-27 19:30:12 -0400

    Initial commit

--- assignment_2/Dnode.java
+++ assignment_2/Dnode.java
@@ -1,11 +1,26 @@
 package assignment_2;
 
+/**
+ * @author chungcl
+ *
+ * @param <E>
+ */
+/**
+ * @author chungcl
+ *
+ * @param <E>
+ */
 public class Dnode<E> implements Position<E>
 {
     private E element;
     private Dnode<E> prev;
     private Dnode<E> next;
 
+    /**
+     * @param e
+     * @param p
+     * @param n
+     */
     public Dnode(E e, Dnode<E> p, Dnode<E> n)
     {
         element = e;
@@ -13,39 +28,65 @@ public class Dnode<E> implements Position<E>
         next = n;
     }
 
+    /**
+     * 
+     */
     public Dnode()
     {
-        element = null;
-        prev = null;
-        next = null;
+    	this(null, null, null);
     }
 
+    /**
+     * @param e
+     */
+    public Dnode( E e ) {
+    	this( e, null, null);
+    }
+    
+    /**
+     * @return
+     */
     public Dnode<E> getPrev()
     {
         return prev;
     }
 
+    /**
+     * @param prev
+     */
     public void setPrev(Dnode<E> prev)
     {
         this.prev = prev;
     }
 
+    /**
+     * @return
+     */
     public Dnode<E> getNext()
     {
         return next;
     }
 
+    /**
+     * @param next
+     */
     public void setNext(Dnode<E> next)
     {
         this.next = next;
     }
 
+    /**
+     * @param element
+     */
     public void setElement(E element)
     {
         this.element = element;
     }
 
-    @Override
+    
+    /**
+     *
+     */
     public E getElement() throws IllegalStateException
     {
         return null;
--- assignment_2/Position.java
+++ assignment_2/Position.java
@@ -3,5 +3,10 @@ package assignment_2;
 public interface Position<E>
 {
 
+    /**
+     * asfdalsdkfj;
+     * @return
+     * @throws IllegalStateException
+     */
     E getElement() throws IllegalStateException;
 }
