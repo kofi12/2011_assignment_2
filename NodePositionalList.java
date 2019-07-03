efd54b9c32eb80eb987df792c9678262d734f010
Aaron Haizel <kofi12@ptl03.eecs.yorku.ca>
2019-07-02 23:29:43 -0400

    added inner class structure found on page 287 of textbook will change if necessary!

--- NodePositionalList.java
+++ NodePositionalList.java
@@ -1,6 +1,7 @@
 package assignment_2;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class NodePositionalList<E> implements PositionalList<E>, Iterable<E>
 {
@@ -102,14 +103,9 @@ public class NodePositionalList<E> implements PositionalList<E>, Iterable<E>
     @Override
     public Iterator<E> iterator()
     {
-        return new ElementIterator<E>();
+        return null; // TODO
     }
 
-    public Iterable<E> positions()
-    {
-        return new NodePositionalList<E>();
-    }
-    
     private DNode<E> validate(Position<E> p) throws IllegalArgumentException
     {
     	if(!(p instanceof DNode)) throw new IllegalArgumentException("Invalid p");
@@ -141,5 +137,58 @@ public class NodePositionalList<E> implements PositionalList<E>, Iterable<E>
         size++;
         return newest;
     }
-    
+
+    private class PositionIterator implements Iterator<Position<E>>
+    {
+        private Position<E> cursor = first();
+        private Position<E> recent = null;
+
+        @Override
+        public boolean hasNext()
+        {
+            return cursor != null;
+        }
+
+        public Position<E> next() throws NoSuchElementException
+        {
+            if (cursor == null) throw new NoSuchElementException("nothing left");
+            recent = cursor;
+            cursor = after(cursor);
+            return recent;
+        }
+
+        public void remove() throws UnsupportedOperationException
+        {
+
+        }
+    }
+
+    private class PositionIterable implements Iterable<Position<E>>
+    {
+        public Iterator<Position<E>> iterator()
+        {
+            return new PositionIterator();
+        }
+    }
+
+    public Iterable<Position<E>> positions()
+    {
+        return new PositionIterable();
+    }
+    /*private class ElementIterator<E> implements Iterator<E>
+    {
+        Iterator<Position<E>> posIterator = new PositionIterator();
+
+        public boolean hasNext()
+        {
+            return posIterator.hasNext();
+        }
+
+        public E next()
+        {
+            return posIterator.next().getElement();
+        }
+
+    }*/
+
 }
